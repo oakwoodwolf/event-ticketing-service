f96ce441d0a15e7a85fa620a0641706538729583
Roy Flaherty <royfla100@gmail.com>
2025-03-04 15:16:57 +0000

    attempt hash router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Client from './Routes/Client';
 import Agent from './Routes/Agent';
 import AdminPage from './Routes/AdminPage';
